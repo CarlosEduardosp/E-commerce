61209cdfa4c30ca6afbe6e2ea7969be9794361ba
Carlos Eduardo - Kadu <carlos.spadilha@yahoo.com.br>
2023-02-23 17:44:21 -0300

    app.py

--- app.py
+++ app.py
@@ -1120,7 +1120,7 @@ def editar_perfil(id_cliente):
 
 #inicializar aplicação
 
-if __name__ == '__main__':
+if __name__ == '__app__':
     app.run(debug=True, host="0.0.0.0", port=5000)
     with app.app_context():
         db.create_all()
